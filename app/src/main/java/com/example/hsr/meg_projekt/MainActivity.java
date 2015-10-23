64c1898ed715b3fa99064396615de6bc2942e075
p-raths <piroh1990@gmail.com>
2015-10-23 12:08:30 +0200

    Login Button "funktioniert"

--- app/src/main/java/com/example/hsr/meg_projekt/MainActivity.java
+++ app/src/main/java/com/example/hsr/meg_projekt/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.hsr.meg_projekt;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -16,6 +19,20 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this, ItemOverview.class);
+
+
+
+
+        EditText editText = (EditText) findViewById(R.id.username);
+        String message = editText.getText().toString();
+
+            startActivity(intent);
 
 
     }
