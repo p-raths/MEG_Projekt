226735dfbf8bf64e55267033eadda890a307727a
p-raths <piroh1990@gmail.com>
2015-10-26 08:45:36 +0100

    Activitys Updated

--- app/src/main/java/com/example/hsr/meg_projekt/Item_Detail.java
+++ app/src/main/java/com/example/hsr/meg_projekt/Item_Detail.java
@@ -1,40 +1,29 @@
 package com.example.hsr.meg_projekt;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity{
+public class Item_Detail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_item__detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-    }
-
-    public void login(View view){
-        Intent intent = new Intent(this, Item_Overview.class);
-
-
-
-
-        EditText editText = (EditText) findViewById(R.id.username);
-        String message = editText.getText().toString();
-
-            startActivity(intent);
-
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
