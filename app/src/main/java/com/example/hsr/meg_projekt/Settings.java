a10881426de859b1df1735e88a9a584bab3e49eb
p-raths <piroh1990@gmail.com>
2015-10-27 11:01:53 +0100

    Settings work now :D

--- app/src/main/java/com/example/hsr/meg_projekt/Settings.java
+++ app/src/main/java/com/example/hsr/meg_projekt/Settings.java
@@ -1,11 +1,16 @@
 package com.example.hsr.meg_projekt;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.example.hsr.meg_projekt.service.LibraryService;
 
 public class Settings extends AppCompatActivity {
 
@@ -16,6 +21,16 @@ public class Settings extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        SharedPreferences prefs = this.getSharedPreferences(
+                "server", Context.MODE_PRIVATE);
+
+        String server = prefs.getString("server", "");
+
+        TextView textView = (TextView) findViewById(R.id.text_Server);
+        textView.setText(server);
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +39,18 @@ public class Settings extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+
+    public void submit(View view){
+
+        EditText serverEdit = (EditText) findViewById(R.id.text_Server);
+        String server = serverEdit.getText().toString();
+
+        LibraryService.setServerAddress(server);
+        Snackbar.make(view, "Server changed", Snackbar.LENGTH_LONG).show();
+
+
     }
 
 }
